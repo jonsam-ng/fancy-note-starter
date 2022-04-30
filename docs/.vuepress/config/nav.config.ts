9c045427b8d3e031d48e8a5922c4f926704a738f
wuqingshan <wuqingshan@xylink.com>
2022-04-30 15:55:01 +0800

    feat: 🆕 support slidev

--- docs/.vuepress/config/nav.config.ts
+++ docs/.vuepress/config/nav.config.ts
@@ -3,20 +3,27 @@
  ******************************************************************************************/
 
 export default [
-  // ========== 示例 ========== //
-  {
-    text: "示例",
-    link: "/example/",
-  },
-  { text: "关于", link: "/about/" },
-  {
-    text: "索引",
-    link: "/archives/",
-    items: [
-      { text: "分类", link: "/categories/" },
-      { text: "标签", link: "/tags/" },
-      { text: "归档", link: "/archives/" },
-    ],
-  },
-  { text: "博客", link: "https://www.jonsam.site" },
+	// ========== 示例 ========== //
+	{
+		text: "示例",
+		link: "/example/",
+	},
+	{
+		text: "索引",
+		link: "/archives/",
+		items: [
+			{ text: "分类", link: "/categories/" },
+			{ text: "标签", link: "/tags/" },
+			{ text: "归档", link: "/archives/" },
+		],
+	},
+	{
+		text: "更多",
+		items: [
+			{ text: "关于", link: "/about/" },
+			// generate by slidev
+			{ text: "幻灯片", link: `/slides/` },
+		],
+	},
+	{ text: "博客", link: "https://www.jonsam.site" },
 ];
--- docs/.vuepress/config/site.config.ts
+++ docs/.vuepress/config/site.config.ts
@@ -5,10 +5,10 @@
 export const siteName = "Fancy Note Starter";
 
 export const siteDesc =
-  "A simple starter to create note site using vuepress and github pages.";
+	"A simple starter to create note site using vuepress and github pages.";
 
 // 是否使用 github pages 的自定义域名
-export const useCustomDomainName = false;
+export const useCustomDomainName = true;
 
 export const siteKeywords = "笔记模板,静态站点";
 
@@ -22,20 +22,20 @@ export const siteRepo = "fancy-note-starter";
 export const personalLink = "https://www.jonsam.site";
 
 export const siteAuthor = {
-  name: "jonsam",
-  link: "https://github.com/jonsam-ng",
+	name: "jonsam",
+	link: "https://github.com/jonsam-ng",
 };
 
 export const siteBlogger = {
-  avatar:
-    "https://www.jonsam.site/wp-content/uploads/2019/10/c1fbc401c64937aaec6cb53359e7c57eabd8e477-e1570649725791.gif",
-  name: "Jonsam NG",
-  slogan: "让有意义的事变得有意思，让有意思的事变得有意义",
+	avatar:
+		"https://www.jonsam.site/wp-content/uploads/2019/10/c1fbc401c64937aaec6cb53359e7c57eabd8e477-e1570649725791.gif",
+	name: "Jonsam NG",
+	slogan: "让有意义的事变得有意思，让有意思的事变得有意义",
 };
 
 export const siteFooter = {
-  createYear: 2022,
-  copyrightInfo: `${siteName} | Made by <a href=${personalLink} target="_blank">${siteAuthor.name}</a> by ❤`,
+	createYear: 2022,
+	copyrightInfo: `${siteName} | Made by <a href=${personalLink} target="_blank">${siteAuthor.name}</a> by ❤`,
 };
 
 export const siteBase = useCustomDomainName ? "" : `/${siteRepo}/`;
